fe3d6176f43860baa2f002871587c7ba9472c5e7
ChrisM2309 <chris.marroquin2309@gmail.com>
2026-06-01 19:02:48 -0600

    create user

---
+++ src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Get, Param, Body, Put, Delete, } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, } from '@nestjs/swagger';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
+@UseGuards(JwtAuthGuard)
+@ApiTags('usuarios')
+@Controller('users')
+export class UsersController {
+    constructor(private readonly usersService: UsersService) { }
+    
+    @Post()
+    @ApiOperation({ summary: 'Crear un nuevo usuario' })
+    @ApiResponse({ status: 201, description: 'Usuario creado correctamente.' })
+    create(@Param('name') name: string, @Param('email') email: string): Promise<User> {
+        return this.usersService.createUser(name, email);
+    }
+}
